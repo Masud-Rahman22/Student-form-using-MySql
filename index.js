389a84249bf7bce7b6c63439d93c715dbb1eca42
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-02-29 00:55:41 +0600

    deployed successfully

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const cors = require('cors');
-const mysql = require('mysql');
 const app = express();
+var cors = require('cors');
+const mysql = require('mysql');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
